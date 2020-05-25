e24c787694cc5f71a7227c33f043531bd58ebf93
Guetso <legalloisprovensal@gmail.com>
2020-05-25 16:22:41 +0200

    add controllers

---
+++ controllers/sauce.js
@@ -0,0 +1,38 @@
+// Contient la logique métier
+
+const Sauce = require('../models/Sauce')
+
+exports.createSauce = (req, res, next) => {
+  delete req.body.userId
+  const sauce = new Sauce({
+    ...req.body,
+  })
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
+    .catch((error) => res.status(400).json({ error }))
+}
+
+exports.modifySauce = (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Sauce modifié !' }))
+    .catch((error) => res.status(400).json({ error }))
+}
+
+exports.deleteSauce = (req, res, next) => {
+  Sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Sauce supprimé !' }))
+    .catch((error) => res.status(400).json({ error }))
+}
+
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }))
+}
+
+exports.getAllSauce = (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }))
+}
