cb31ee8fc227eb40e4d131747632b3b62a8b0bfc
Guetso <legalloisprovensal@gmail.com>
2020-05-25 17:00:10 +0200

    add auth logic

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 const sauceRoutes = require('./routes/sauce')
+const userRoutes = require('./routes/user')
 
 mongoose
   .connect(
@@ -30,5 +31,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 
 app.use('/api/sauces', sauceRoutes)
+app.use('/api/auth', userRoutes)
 
 module.exports = app
